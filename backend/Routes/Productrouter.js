f0f06a9a67674e20e1b783bad8333d6daa03cf4e
Azmir02 <azmir02alif@gmail.com>
2022-03-27 18:32:21 +0600

    change1

--- backend/Routes/Productrouter.js
+++ backend/Routes/Productrouter.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import productcontroller from '../Controller/productdetailscontroll.js'
 import idcontroller from '../Controller/Idcontroller.js'
+import popupgetcontroller from '../Routes/Popupgetcontroller.js'
+import psliders from '../Controller/Productslidecontroll.js'
 
 const Productrouter = express.Router()
 
 
+Productrouter.get('/',popupgetcontroller)
 Productrouter.get('/products',productcontroller)
 Productrouter.get('/:id',idcontroller)
+Productrouter.get('/pslide',psliders)
 
 
 
